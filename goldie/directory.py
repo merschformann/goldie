40abc4449da4dcd1ef1d32b2fd435d15347675b9
Marius Merschformann <marius.merschformann@gmail.com>
2025-03-10 00:28:06 +0100

    Working on test execution

--- goldie/directory.py
+++ goldie/directory.py
@@ -1,3 +1,7 @@
+import glob
+import inspect
+import os.path
+import unittest
 from dataclasses import dataclass
 
 
@@ -7,3 +11,72 @@ class Configuration:
 
     directory: str
     file_filter: str
+
+
+def _get_golden_filename(path: str) -> str:
+    """
+    Get the golden filename from a path.
+
+    Parameters
+    ----------
+    path : str
+        The path to get the golden filename from.
+
+    Returns
+    -------
+    str
+        The golden filename.
+    """
+    name, ext = os.path.splitext(path)
+    name = os.path.basename(name)
+    return f"{name}.golden{ext}"
+
+
+def _get_caller_directory() -> str:
+    """
+    Get the directory of the caller.
+
+    Returns
+    -------
+    str
+        The directory of the caller.
+    """
+    # Get the current stack frame
+    current_frame = inspect.currentframe()
+    # Get the frame of the caller
+    caller_frame = current_frame.f_back
+    # Get the filename of the caller
+    caller_filename = caller_frame.f_code.co_filename
+    # Get the directory of the caller
+    caller_directory = os.path.dirname(caller_filename)
+    return caller_directory
+
+
+def run(
+    test: unittest.TestCase,
+    configuration: Configuration,
+):
+    """
+    Run the golden file test.
+
+    Parameters
+    ----------
+    test : unittest.TestCase
+        The test case to run.
+    configuration : Configuration
+        The configuration for the golden file test.
+    """
+
+    # Find all relevant files in the directory
+    test_files = glob.glob(os.path.join(_get_caller_directory(), configuration.directory, configuration.file_filter))
+
+    # Iterate over the test cases
+    for i, (test_file) in enumerate(test_files):
+        with test.subTest(file=test_file):
+            # Get the golden file
+            golden_file = _get_golden_filename(test_file)
+
+            # Read the test file
+
+            # Assert the content
+            test.assertEqual(actual, expected, f"Test case {i} failed.")
---
+++ goldie/run.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class OutputMode(Enum):
+    STDOUT = "stdout"
+    """Intercepts only stdout."""
+    STDERR = "stderr"
+    """Intercepts only stderr."""
+    BOTH = "both"
+    """Intercepts both stdout and stderr."""
+
+
+@dataclass
+class Configuration:
+    cmd: str
+    """The command to run."""
+    args: list[str]
+    """
+    The arguments to pass to the command.
+    If the path to the input file is needed (instead of feeding via stdin), use the string "{input}".
+    If the path to the output file is needed (instead of reading stdout), use the string "{output}".
+    """
+    use_stdin: bool = True
+    """Whether to use stdin for input."""
+    use_stdout: bool = True
+    """Whether to use stdout for output."""
+    output_mode: OutputMode = OutputMode.STDOUT
+    """Indicates how to intercept the output."""
+
+
+def run(
+    input_file: str,
+    output_file: str,
