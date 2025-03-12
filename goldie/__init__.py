a311c164458c6d27b1f294151e2e7adee739ee72
Marius Merschformann <marius.merschformann@gmail.com>
2025-03-12 04:50:17 +0100

    Support single file tests

--- goldie/__init__.py
+++ goldie/__init__.py
@@ -11,13 +11,15 @@ from .comparison import RegexReplacement as RegexReplacement
 from .comparison import compare as compare
 from .diff import Difference as Difference
 from .diff import DiffStyle as DiffStyle
-from .directory import ConfigDirectoryTest as ConfigDirectoryTest
-from .directory import TestDefinition as TestDefinition
-from .directory import run_unittest as run_unittest
 from .execution import ConfigRun as ConfigRun
 from .execution import ConfigRunValidation as ConfigRunValidation
 from .execution import InputMode as InputMode
 from .execution import OutputMode as OutputMode
 from .execution import execute as execute
+from .testing import ConfigDirectoryTest as ConfigDirectoryTest
+from .testing import ConfigFileTest as ConfigFileTest
+from .testing import TestDefinition as TestDefinition
+from .testing import run_directory_unittest as run_directory_unittest
+from .testing import run_file_unittest as run_file_unittest
 
 VERSION = __version__
